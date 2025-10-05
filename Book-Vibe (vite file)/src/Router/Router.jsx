56a810218a03ece4a3439ea803340e5d2b70c394
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2025-10-05 17:10:42 +0600

    complait the header and footer section

--- Book-Vibe (vite file)/src/Router/Router.jsx
+++ Book-Vibe (vite file)/src/Router/Router.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root";
+import Error from "../Components/error/Error";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello Worldfhjkffkjsh</div>,
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
   },
 ]);
