41abdaf7bc8bcc8c01056368bfa7ca5d2c46c59e
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2025-10-05 15:26:29 +0600

    making a file and exporting the router

--- Book-Vibe (vite file)/src/main.jsx
+++ Book-Vibe (vite file)/src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { createBrowserRouter } from "react-router";
+import App from "./App";
 import { RouterProvider } from "react-router/dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { router } from "./Router/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
