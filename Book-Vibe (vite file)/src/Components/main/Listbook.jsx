c4bebcfcd7813742dbb5bc9304266148e03802b2
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2025-10-07 01:53:16 +0600

    some desing on list book

--- Book-Vibe (vite file)/src/Components/main/Listbook.jsx
+++ Book-Vibe (vite file)/src/Components/main/Listbook.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+//import React, { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Listbook = () => {
-  return <div>i am list</div>;
+  //const [value, setValue] = useState(false);
+
+  return (
+    <div>
+      <div className="font-extrabold text-2xl py-6 bg-gray-300 rounded-3xl flex justify-center items-center">
+        <p className="text-center">Books</p>
+      </div>
+      <div className="dropdown dropdown-bottom dropdown-center">
+        <div
+          tabIndex={0}
+          role="button"
+          className=" m-1 btn mx-5 bg-green-600 text-neutral-50"
+        >
+          Sort By :
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <a>Pages</a>
+          </li>
+          <li>
+            <a>review</a>
+          </li>
+        </ul>
+      </div>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Listbook;
